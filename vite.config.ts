0d09b61d00a3ac2fb0380386f0738dd387d11439
mahmoud shahieen <mahmoudshahieen4.com>
2026-06-23 02:32:13 +0200

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; 
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react({
-      // نقوم بتمرير الـ React Compiler مباشرة داخل إعدادات Babel الخاصة بملحق ريآكت
-      babel: {
-        plugins: [
-          ["babel-plugin-react-compiler", {}],
-        ],
-      },
-    }),
-  ],
-})
+  plugins: [react()],
+});
